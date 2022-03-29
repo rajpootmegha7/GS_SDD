23f65b7cf6bc49b917fc2e0e2c13d3726fb192ea
ottdog <55850761+ottdog@users.noreply.github.com>
2022-03-29 11:48:16 -0400

    Added doccumentation

--- server/forgot_password.js
+++ server/forgot_password.js
@@ -5,18 +5,24 @@ const pool = require('./connection/connectPSQL')
 
 //http://localhost:4000/forgot/api/check_userid
 forgot_router.post('/api/check_userid', function (req, res) {
+    //get username data from the frontend
     var user_data = req.body;
     console.log(user_data);
     var _username = user_data._username;
 
+    //query for the database to find users with username.
     var security_query = "SELECT * FROM public.userinfo WHERE user_name='" + _username + "'";
     console.log(security_query);
-
+    
+    //connect to server
     pool.connect((err,db,done)=>{
+        //fail
         if (err) {
             (console.log('Error in Connecting the POOL: ' + err));
             return res.status(400).send(err);
         }
+        
+        //after connection, ask for users with username
         db.query(security_query,(err, table) => {
 
             done();
@@ -25,15 +31,13 @@ forgot_router.post('/api/check_userid', function (req, res) {
             if(table.rowCount === 0){
                 return res.status(406).send({text:"Username does not match. Please ensure that fields are correct."});
             }
-            // Username is valid
+            // A username was found, return security question
             else{
                 var question = '';
                 table.rows.forEach(row =>{
                     console.log(row.security_question)
                     question = row.security_question
                 })
-
-                
                 return res.status(200).send({text:question});
             }
         })
