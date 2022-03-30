1cf8ad67d9e82b97ec22999bda9ee5fe9077f4ff
DoughChild <zinkna1@gmail.com>
2022-03-29 20:34:46 -0400

    Added links to footer, removed comments

--- client/gardenshepherd/src/components/Footer/Footer.js
+++ client/gardenshepherd/src/components/Footer/Footer.js
@@ -16,47 +16,32 @@ export default class Footer extends Component {
                     <h2>Resources</h2>
                     <div id="modules">
                         <h2>Resources</h2>
-                        {/*<div class="module">
-                            <h3>Product</h3>
-                            <ul>
-                                <li><a href="">Pricing</a></li>
-                                <li><a href="">Features</a></li>
-                                <li><a href="">Documentation</a></li>
-                                <li><a href="">Developers</a></li>
-                            </ul>
-                        </div>*/}
                         <div class="module">
                             <h3>Company</h3>
                             <ul>
-                                <li><a href="">Mission</a></li>
-                                <li><a href="">History</a></li>
-                                <li><a href="">Staff</a></li>
-                                <li><a href="">Management</a></li>
-                                <li><a href="">Contact</a></li>
+                                <li><a href="/gs/about-us">Mission</a></li>
+                                <li><a href="/gs/about-us">Staff</a></li>
+                                <li><a href="/gs/planner">Planner</a></li>
+                                <li><a href="/gs/dashboard">Encyclopedia</a></li>
+                                <li><a href="/logout">Logout</a></li>
                             </ul>
                         </div>
-                        {/*<div class="module">
-                            <h3>Contact</h3>
-                            <ul>
-                                <li class="mail"><a href="">Support</a></li>
-                                <li class="phone"><a href="">+0 1 234 567</a></li>
-                                <li class="twitter"><a href="">@company</a></li>
-                                <li class="facebook"><a href="">Company</a></li>
-                            </ul>
-                        </div>*/}
                         <div class="module">
-                            <h3>Company</h3>
-                            <p>Text area used for company description, address and company logo.</p>
+                            <h3>Garden Shepherd</h3>
+                            <p>
+                                Here at Garden Shepherd, we pride ourselves in our ability to help anyone with their forays into gardening.
+                                Whether its your first time or your fiftieth, our app will provide you with the tools you need to create a garden and mantain it for years!
+                            </p>
                             <ul>
-                                <li>Company street 123</li>
-                                <li>123 45 City, Country</li>
+                                <li>110 8th Street</li>
+                                <li>Troy, NY 12180</li>
                             </ul>
                         </div>
                     </div>
                 </section>
                 <div id="signature" aria-hidden="true"></div>
                 <footer id="legal">
-                    <p>&copy; 3000 by Company. All rights reserved. Please read the <a href="">Privacy Policy</a> and <a href="">Terms of Service</a>.</p>
+                    <p>&copy; 2022 by Garden Shepherd. All rights reserved.</p>
                 </footer>
             </div>
         )
